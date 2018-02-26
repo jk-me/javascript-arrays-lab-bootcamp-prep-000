ba5ad8df8aa49d977e84f13f77e2def4489ce2d9
Jenny Kam <jkam17@gmail.com>
2018-02-26 03:09:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const app = "I don't do much."
 window.kittens = ['Milo', 'Otis', 'Garfield']
 
 kittens = function(){
-  var kittens = 
+  var kittens = ['Milo', 'Otis', 'Garfield']
 }
-describe('kittens', function() {
-    it('defines kittens as `var kittens = ["Milo", "Otis", "Garfield"]`', function() {
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
-    })
-  })
+
+function destructivelyAppendKitten(n){
+  
+}
 
   describe('destructivelyAppendKitten(name)', function() {
     it('appends a kitten to the end of the kittens array', function() {
